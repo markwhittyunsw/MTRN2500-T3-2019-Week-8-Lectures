e6a0fac93c3944bb4df9394515d1ac7c3bff6960
markwhittyunsw <m.whitty@unsw.edu.au>
2019-11-07 09:10:44 +1100

    Generated constCast.cpp

---
+++ constCast.cpp
@@ -0,0 +1,15 @@
+// const_cast
+// Example from cplusplus.com
+#include <iostream>
+using namespace std;
+
+void print (char * str)
+{
+  cout << str << '\n';
+}
+
+int main () {
+  const char * c = "sample text";
+  print ( const_cast<char *> (c) );
+  return 0;
+}

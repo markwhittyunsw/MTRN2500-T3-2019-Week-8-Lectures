edcae676b38a538106f1bdfa1e93dabb533d9292
markwhittyunsw <m.whitty@unsw.edu.au>
2019-11-07 11:16:36 +1100

    Creating testTypeID.cpp

---
+++ testTypeID.cpp
@@ -0,0 +1,19 @@
+// typeid test
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+class Test {};
+
+int main () {
+  int * a,b;
+  Test T;
+  a=0; b=0;
+  if (typeid(a) != typeid(b))
+  {
+    std::cout << "a and b are of different types:" << std::endl;
+    std::cout << "a is: " << typeid(a).name() << std::endl;
+    std::cout << "b is: " << typeid(b).name() << std::endl;
+    std::cout << "T is: " << typeid(T).name() << std::endl;
+  }
+  return 0;
+}

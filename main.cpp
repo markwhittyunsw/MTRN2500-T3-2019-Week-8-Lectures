29ead48e5788db3fc5ad71487c15e480dd939313
markwhittyunsw <m.whitty@unsw.edu.au>
2019-11-08 12:23:46 +1100

    Class derivation examples

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+#include "Point.h"
+#include "Circle.h"
+
+using namespace std;
+
+int main()
+{
+	Point Centre(10.5, 9.76);
+	
+	cout << "[x,y] = [" << Centre.GetX() << ", " << Centre.GetY() << "]" << endl;
+	Circle C1(Centre, 10.0);
+
+	cout << "Circle C1 parameters [x, y, r] are [" << C1.GetX() << ", " << C1.GetY() << ", " << C1.GetRadius() << "]" << endl;
+	
+	Circle C2(1, 2, 3);
+
+	cout << "Circle C2 parameters [x, y, r] are [" << C2.GetX() << ", " << C2.GetY() << ", " << C2.GetRadius() << "]" << endl;
+	
+	Circle C3();
+	cout << "Circle C3 parameters [x, y, r] are [" << C3.GetX() << ", " << C3.GetY() << ", " << C3.GetRadius() << "]" << endl;
+
+	
+	_getch();
+	return 0;
+
+}
+
